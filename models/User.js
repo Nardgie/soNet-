407469316051568b8d4afddb22403d8c288c7fc1
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-05-15 20:56:33 -0400

    started User model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    // create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+    toJSON: {
+        virtuals: true
+
+    },
+    id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
